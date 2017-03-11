544832255930e2a09dfdf844e20951d797bb671d
nate6631 <nate6631@gmail.com>
2017-03-11 11:22:56 -0600

    Created Drawer List String Items

--- app/src/main/java/net/skyestudios/mtgcardquery/MainActivity.java
+++ app/src/main/java/net/skyestudios/mtgcardquery/MainActivity.java
@@ -2,13 +2,24 @@ package net.skyestudios.mtgcardquery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private ListView drawerList;
+    private ArrayAdapter<String> drawerListAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_drawer);
+
+
+        drawerListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.drawerListStrings));
+        drawerList = (ListView) findViewById(R.id.drawerList);
+        drawerList.setAdapter(drawerListAdapter);
 
     }
 }
