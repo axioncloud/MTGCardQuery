4219dd7ece71b0506f6c1c771225a08b50acca02
Nathaniel Martin <arkeonet64@gmail.com>
2017-05-05 13:07:50 -0500

    Renamed deck and wishlist activities to plural added DeckEditorActivity

---
+++ app/src/main/java/net/skyestudios/mtgcardquery/DeckEditorActivity.java
@@ -0,0 +1,15 @@
+package net.skyestudios.mtgcardquery;
+import android.support.v7.app.AppCompatActivity;
+import android.os.*;
+
+public class DeckEditorActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		//TODO: Implement this method
+	}
+	
+}
--- app/src/main/java/net/skyestudios/mtgcardquery/WishlistsViewActivity.java
+++ app/src/main/java/net/skyestudios/mtgcardquery/WishlistsViewActivity.java
@@ -3,11 +3,11 @@ package net.skyestudios.mtgcardquery;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class DeckViewActivity extends AppCompatActivity {
+public class WishlistsViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deck_view);
+        setContentView(R.layout.activity_wishlist_view);
     }
 }
