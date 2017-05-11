092523909a2898d543167489649d9857e39f8941
Nathaniel Martin <nate6631@gmail.com>
2017-05-11 05:37:12 -0500

    Added mana icons

--- app/src/main/java/net/skyestudios/mtgcardquery/adapters/CardViewAdapter.java
+++ app/src/main/java/net/skyestudios/mtgcardquery/adapters/CardViewAdapter.java
@@ -109,36 +109,140 @@ public class CardViewAdapter extends ArrayAdapter<CardView> {
                     manaCostHolder.addView(manaImageView);
                     break;
                 case "{5}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_5));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_5));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{6}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_6));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_6));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{7}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_7));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_7));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{8}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_8));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_8));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{9}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_9));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_9));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{10}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_10));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_10));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{11}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_11));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_11));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{12}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_12));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_12));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{13}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_13));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_13));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{14}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_14));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_14));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{15}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_15));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_15));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{16}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_16));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_16));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{17}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_17));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_17));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{18}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_18));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_18));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{19}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_19));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_19));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{20}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_20));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_20));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+                case "{X}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_x));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_x));
+                    }
+                    manaCostHolder.addView(manaImageView);
                     break;
                 case "{R}":
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -187,6 +291,80 @@ public class CardViewAdapter extends ArrayAdapter<CardView> {
                         }
                         manaCostHolder.addView(manaImageView);
                     }
+                    break;
+
+                case "{2/B}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_2b));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_2b));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+                case "{2/G}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_2g));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_2g));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+                case "{2/U}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_2u));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_2u));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+                case "{2/R}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_2r));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_2r));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+                case "{2/W}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_2w));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_2w));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+
+                case "{B/G}":
+                    break;
+                case "{B/U}":
+                    break;
+                case "{B/R}":
+                    break;
+                case "{B/W}":
+                    break;
+
+                case "{G/B}":
+                    break;
+                case "{G/U}":
+                    break;
+                case "{G/R}":
+                    break;
+                case "{G/W}":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        manaImageView.setImageDrawable(getContext().getDrawable(R.drawable.ic_gw));
+                    } else {
+                        manaImageView.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_gw));
+                    }
+                    manaCostHolder.addView(manaImageView);
+                    break;
+
+                case "{U/G}":
+                    break;
+                case "{U/B}":
+                    break;
+                case "{U/R}":
+                    break;
+                case "{U/W}":
                     break;
                 default:
                     break;
